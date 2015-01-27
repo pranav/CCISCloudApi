836811f1a357acd50bb0b0bce82ea4253e59cad4
Pranav Gandhi <pgandhi@hubspot.com>
2015-01-27 00:43:41 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cciscloud',
+    version='1.0',
+    description='Portal for the CCIS Cloud running on Amazon',
+    author='Pranav Gandhi',
+    author_email='hyfi@ccs.neu.edu',
+    packages=find_packages()
+)
